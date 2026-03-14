80687d16d4ae9af510319e03a0229833951b29c4
leffty <brigante2004@yandex.ru>
2026-03-14 12:03:34 +0300

    Add FavouriteGenre model

--- backend/genre/models.py
+++ backend/genre/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 from .constants import (
     GENRE_DESCRIPTION_MAX_LENGTH,
     GENRE_NAME_MAX_LENGTH,
 )
+
+User = get_user_model()
 
 
 class Genre(models.Model):
@@ -36,3 +39,30 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class FavouriteGenre(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+        related_name='user_favourite_genres',
+    )
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+        verbose_name='Жанр',
+    )
+
+    class Meta:
+        verbose_name = 'Любимый жанр'
+        verbose_name_plural = 'Любимые жанры'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'genre'],
+                name='unique_%(class)s'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user.username} - {self.genre.name}'
