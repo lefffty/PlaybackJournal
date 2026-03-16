aa9347f9dd945e6d1d5fbe0799c6bf3cc5429a0a
leffty <brigante2004@yandex.ru>
2026-03-16 11:47:52 +0300

    Create RatedSong model and register it in admin-zone

--- backend/song/admin.py
+++ backend/song/admin.py
@@ -1,16 +1,36 @@
 from django.contrib import admin
 
-from .models import Song
+from .models import Song, RatedSong
 from .constants import SONG_LIST_PER_PAGE
 from .inlines import SongArtistInline
 
 
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'duration',)
-    search_fields = ('name',)
+    list_display = (
+        'id',
+        'name',
+        'duration',
+    )
+    search_fields = (
+        'name',
+    )
     list_per_page = SONG_LIST_PER_PAGE
     ordering = (
         'id',
     )
     inlines = [SongArtistInline]
+
+
+@admin.register(RatedSong)
+class RatedSongAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'user',
+        'song',
+        'rating'
+    )
+    search_fields = (
+        'user__username',
+        'song__name',
+    )
--- backend/song/models.py
+++ backend/song/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 from .constants import SONG_NAME_MAX_LENGTH
+
+User = get_user_model()
 
 
 class Song(models.Model):
@@ -25,3 +29,37 @@ class Song(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class RatedSong(models.Model):
+    song = models.ForeignKey(
+        Song,
+        on_delete=models.CASCADE,
+        verbose_name='Песня'
+    )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь',
+    )
+    rating = models.IntegerField(
+        verbose_name='Оценка',
+        default=0,
+        validators=[
+            MaxValueValidator(10),
+            MinValueValidator(0),
+        ]
+    )
+
+    class Meta:
+        verbose_name = 'Оценка песни'
+        verbose_name_plural = 'Оценки песен'
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'song'],
+                name='unique_%(class)s',
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user.username} - {self.song.name} - {self.rating}'
