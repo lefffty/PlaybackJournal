467f7c79e7fae326e8fc65e97cacbfd1b426feca
leffty <brigante2004@yandex.ru>
2026-03-18 10:34:37 +0300

    Register models Review and ReviewComment in admin-zone

--- backend/review/admin.py
+++ backend/review/admin.py
@@ -1,3 +1,72 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Review,
+    ReviewComment,
+)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'text',
+        'type',
+        'author',
+        'album',
+        'created_at',
+        'updated_at',
+    )
+    list_filter = (
+        'type',
+        'album',
+    )
+    search_fields = (
+        'title',
+        'text',
+        'author__username',
+        'album__name',
+    )
+    readonly_fields = (
+        'display_comments_count',
+    )
+    fieldsets = (
+        (None, {
+            "fields": (
+                'author',
+                'title',
+                'text',
+                'type',
+                'album',
+            ),
+        }),
+        ("Статистика", {
+            'fields': (
+                'display_comments_count',
+            ),
+        }),
+    )
+
+    @admin.display(description='Количество комментариев к рецензии')
+    def display_comments_count(self, obj: Review):
+        return obj.comments.count()
+
+
+@admin.register(ReviewComment)
+class ReviewCommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'text',
+        'author',
+        'review',
+        'created_at',
+    )
+    search_fields = (
+        'text',
+        'author__username',
+        'review__title',
+    )
+    fields = (
+        'author',
+        'text',
+        'review',
+    )
